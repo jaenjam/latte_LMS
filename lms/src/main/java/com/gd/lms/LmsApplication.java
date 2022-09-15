e08bd2500f058341f01608f654ce9f0c085d4dd9
huiwon-kim <kjo109@naver.com>
2022-09-15 09:48:30 +0900

    [김희원]_2022.09.15_ utf-8EncodingFilter 적용

--- lms/src/main/java/com/gd/lms/LmsApplication.java
+++ lms/src/main/java/com/gd/lms/LmsApplication.java
@@ -2,8 +2,11 @@ package com.gd.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
+
+@ServletComponentScan // 서블릿에서 사용하는 webServlet listener filter같은 것들 사용할수있게해줌
+@SpringBootApplication // 이거때문에 모든걸 다 쓸 수 있음 BUT, 기존에 웹 서블릿과 리스너를 쓸수있는건 들어있지않아서 추가해줘야함ServletComponentScan
 public class LmsApplication {
 
 	public static void main(String[] args) {
---
+++ lms/src/main/java/com/gd/lms/filter/EncodingFilter.java
@@ -0,0 +1,23 @@
+package com.gd.lms.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+}
