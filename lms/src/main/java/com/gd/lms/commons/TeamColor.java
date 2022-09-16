097b76a03efd1c7a1a9131e89a48bdeae9869ee0
huiwon-kim <kjo109@naver.com>
2022-09-16 17:43:24 +0900

    [김희원]_2022.09.16_Teamcolor추가

---
+++ lms/src/main/java/com/gd/lms/commons/TeamColor.java
@@ -0,0 +1,9 @@
+package com.gd.lms.commons;
+
+public class TeamColor {
+	 public static final String KHW ="\u001B[34m";
+	 //						└ 상수라서 대문자랭
+	 public static final String JJY ="\u001B[31m";
+	 public static final String CSJ ="\u001B[35m";
+	 public static final String LJE ="\u001B[33m";
+}
