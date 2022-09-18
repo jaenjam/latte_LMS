d0f2963de036845210a965f9cc34b9f8fcf7886d
JEZEJ <tygns5574@naver.com>
2022-09-18 18:17:25 +0900

    [정지영]_2022.09.18_professor상세보기진행중(쿼리MAPPER완성)

--- lms/src/main/java/com/gd/lms/mapper/ProfessorMapper.java
+++ lms/src/main/java/com/gd/lms/mapper/ProfessorMapper.java
@@ -1,5 +1,7 @@
 package com.gd.lms.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.gd.lms.vo.Professor;
@@ -12,4 +14,7 @@ public interface ProfessorMapper {
 
 	// 교수추가 (회원가입)
 	int insertProfessor(Professor professor);
+	
+	// 교수상세보기
+	List<Professor> selectProfessorOne(int professorNo);
 }
