9a2220968089dde54944797cc3af3823f73b6d1a
seo-jeong2 <chusi12@naver.com>
2022-09-22 11:56:26 +0900

    [추서정]_2022.09.22_TeamColor변경

--- lms/src/main/java/com/gd/lms/commons/TeamColor.java
+++ lms/src/main/java/com/gd/lms/commons/TeamColor.java
@@ -3,7 +3,7 @@ package com.gd.lms.commons;
 public class TeamColor {
 	 public static final String KHW ="\u001B[34m";
 	 //						└ 상수라서 대문자랭
-	 public static final String JJY ="\u001B[31m";
-	 public static final String CSJ ="\u001B[35m";
+	 public static final String JJY ="\u001B[36m";
+	 public static final String CSJ ="\u001B[32m";
 	 public static final String LJE ="\u001B[33m";
 }
