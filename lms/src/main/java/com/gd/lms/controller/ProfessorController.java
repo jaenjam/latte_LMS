e4397efddc84663b8ffffd52ca4a53b374838745
JEZEJ <tygns5574@naver.com>
2022-09-22 20:53:14 +0900

    [정지영]_2022.09.22_Professor_image/file추가구현중

--- lms/src/main/java/com/gd/lms/controller/ProfessorController.java
+++ lms/src/main/java/com/gd/lms/controller/ProfessorController.java
@@ -1,8 +1,11 @@
 package com.gd.lms.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -155,21 +158,49 @@ public class ProfessorController {
 	}
 
 	// 교수사진등록하기 (Form)
-	@GetMapping("/professor/addProfessorImgForm")
-	public String addProfessorImg(Model model) {
-		log.debug(TeamColor.JJY + "[Controller] addProfessorImg get실행");
-		
-		return "/professor/addProfessorImgForm";
-	}
+		@GetMapping("/professor/addProfessorImgForm")
+		public String addProfessorImg(Model model) {
+			log.debug(TeamColor.JJY + "[Controller] addProfessorImg get실행");
+			
+			return "/professor/addProfessorImgForm";
+		}
 
-	// 교수사진등록하기 (Action) 첨부파일 업로드
-	@PostMapping("/addProfessorImg")
-	public String addProfessorImg(ProfessorImg professorImg, Model model, MultipartFile[] uploadFile) {
+		// 교수사진등록하기 (Action) 첨부파일 업로드
+		@PostMapping("/addProfessorImg")
+		public String addProfessorImg(@RequestParam("file") MultipartFile file) {
 
-		log.debug(TeamColor.JJY + "[Controller] addProfessorImg post실행");
+			log.debug(TeamColor.JJY + "[Controller] addProfessorImg post실행");
+			
+			String fileRealName = file.getOriginalFilename(); //파일명을 나타내줌
+			long size = file.getSize(); //파일사이즈
+			
+			System.out.println("파일명 : "  + fileRealName);
+			System.out.println("용량크기(byte) : " + size);
+			
+			String fileExtension = fileRealName.substring(fileRealName.lastIndexOf("."),fileRealName.length());
+			String uploadFolder = "C:\\test\\upload";
+			
+			UUID uuid = UUID.randomUUID();
+			System.out.println(uuid.toString());
+			String[] uuids = uuid.toString().split("-");
+			
+			String uniqueName = uuids[0];
+			System.out.println("생성된 고유문자열" + uniqueName);
+			System.out.println("확장자명" + fileExtension);
+			
+			// File saveFile = new File(uploadFolder+"\\"+fileRealName); uuid 적용 전
+			
+			File saveFile = new File(uploadFolder+"\\"+uniqueName + fileExtension);  // 적용 후
+			try {
+				file.transferTo(saveFile); // 실제 파일 저장메서드(filewriter 작업을 손쉽게 한방에 처리해준다.)
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-		return "/professor/getProfessorOne";
-	}
+			return "/professor/getProfessorOne";
+		}
 
 	// 교수목록
 	@GetMapping("/professor/professorList")
