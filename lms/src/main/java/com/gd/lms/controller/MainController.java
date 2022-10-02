77614465b3cc47f6db5f3d36f7bc4df443622f85
huiwon-kim <kjo109@naver.com>
2022-10-02 18:29:14 +0900

    [김희원]_2022.10.02_수강신청(Register)진행중6.2?

--- lms/src/main/java/com/gd/lms/controller/MainController.java
+++ lms/src/main/java/com/gd/lms/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.gd.lms.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +19,18 @@ public class MainController {
 	
 		// 상단 헤더 누를 시 메인접근
 		@GetMapping("/home")
-		public String gomain() {
+		public String gomain(HttpServletRequest request) {
 			// 해당컨트롤러 진입여부 확인
 			log.debug(TeamColor.KHW +"메인 이동");
 			
-			return "/home";
+			// 세션 사용하기위해 선언
+			HttpSession session =  request.getSession();	
+			
+			if(session.getAttribute("user") !=null) {
+				return "/home";
+			} else {
+				log.debug(TeamColor.KHW + "로그인 만료되었습니다!");				
+			return "/loginForm";
+			}
 		}
 }
--- lms/src/main/webapp/js/register/Register.js
+++ lms/src/main/webapp/js/register/Register.js
@@ -1,34 +1,81 @@
 
 
 $(document).ready(function() {
-		// 
+		// 문서 시작되면 바로 기능 준비할거양
 
+	// 학점이 select되면
     $('#subjectcredit').change(function() {
 	// getRegisterForm 의 해당 id="" 값이 변경하면
-	// 밑에 ┌ '이거' 실행
+	// 밑에 ┌ '이거' 실행할거다?
         getSubjectName(this.value);
-        //				>>> VIEW단의 OPTION VALUE 가져온다는 소리
+        //				└>>> VIEW단의 OPTION VALUE 가져온다는 소리
+    });
+    
+    
+    // view단의 담기 버튼 누르면
+    $('#addReigisterCart').click(function() {
+		console.log("눌림?");
+    	alert('장바구니에 담습니다!');
+    	addRegisterCart(this.value);
     });
 });
+
 
 // ┌ '이거' 가 뭔지 정의하기
 function getSubjectName(subjectCredit) {
     $.ajax({
-        url: '/api/register/get/subjectName/' + subjectCredit,
+        url: '/api/register/get/subjectName/' + subjectCredit, // apiController와 통신할 주소는 여기야()
         type: "GET",
         contentType: 'application/json',
 
         success: function (data) { //결과성공 콜백함수
             let subjectList = $("#subjectList");
             subjectList.empty();
+            // └ 가져올 리스트 초기화로 비워주기 (for문 돌리니까)
             for(let i = 0; i<data.length; i++){
-
-                subjectList.append("<li>" + "<input onclick='run(this.id)' class=\"subject\" type=\"checkbox\" id=\"chk\">"
-                    + "<p class=\"idbirth_p\">" +  data[i].subject.subjectName + "</p></li>");
+	
+					//		┌ 다 가져와서 재정의 하려고 append 쓰는거야
+                subjectList.append("<li>" + "<input onclick='run(this.id)' class=\"subject\" type=\"checkbox\" id=\"chk\" name=\"chk\">"
+                    + "<p class=\"idbirth_p\" name=\"idbirth_n\" id=\"idbirth_i\"value=\"idbirth_v\">" 
+                    + " 과목명: "+  data[i].subject.subjectName 
+                    + "　전공여부: " +data[i].subject.subjectMajor
+                    +  "　/　요일: " + data[i].day
+                    +  "　/　교시: " + data[i].startTime + "~" + data[i].endTime
+                    +  "　/　담당교수: " + data[i].professor.professorName
+                    +  " </p></li>");
+           
+           console.log(data[i])
             }
         },
         error: function (request, status, error) { // 결과에러 콜백함수
-            alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+        	console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error)
+            alert("과목을 불러올 수 없습니다!");
         }
     });
+} // getSubjectName function end
+
+
+
+
+	
+
+function addRegisterCart() {
+	console.log("들어옴?")
+	var subjectCk = [];
+	
+	// 이름이 chk 중 체크된거 위에 선언한 객체에 담을거야
+	$("input[name='chk']:checked").each(function(i){
+		console.log("맞음?")
+		
+		var registerCart = $(idbirth_i).val();
+		
+		// console.log(subjectList) // 이건 empty로 선언해서 비어있어
+		//console.log(idbirth_n)
+		console.log(idbirth_i)
+		//console.log(idbirth_v)
+		
+		console.log("?????"+registerCart)
+	
+
 }
+)};
